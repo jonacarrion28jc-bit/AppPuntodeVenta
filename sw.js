35460c2451c49d7fd79cb375114e06155c7279eb
jonacarrion28jc-bit <jonacarrion28.jc@gmail.com>
2026-09-15 22:21:53 -0600

    Update cache version to 'pos-vitys-v2'

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service worker: cachea la interfaz para que abra al instante.
    Los DATOS nunca se cachean: siempre vienen frescos de la API. */
-const CACHE = 'pos-vitys-v1';
+const CACHE = 'pos-vitys-v2';
 const ARCHIVOS = [
   './',
   './index.html',
